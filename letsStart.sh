1fcbed0b8f20dd139cbafe554fa6a92dc972c28e
igor.budasov <igor@mediamonks.com>
2015-09-17 11:31:54 +0200

    improvements

--- letsStart.sh
+++ letsStart.sh
@@ -1,61 +1,114 @@
 #!/bin/bash
 clear
-sudo apt-get update
-sudo apt-get install software-properties-common python-software-properties -y
-sudo apt-get install subversion -y
-sudo apt-get install curl -y
-sudo apt-get install git -y
-sudo apt-get install vim -y
-sudo apt-get install mc -y
-sudo apt-get install lynx-cur
+export LANGUAGE=en
+export LC_ALL=en_US.UTF-8
+export LANG=en_US.UTF-8
+
+sudo locale-gen en_US.UTF-8
+sudo update-locale en_US
+
+
+sudo aptitude update
+clear
+sudo aptitude install software-properties-common python-software-properties -y
+clear
+sudo aptitude install subversion -y
+clear
+sudo aptitude install curl -y
+clear
+sudo aptitude install git -y
+clear
+sudo aptitude install vim -y
+clear
+sudo aptitude install mc -y
+clear
+sudo aptitude install lynx-cur
 
 # APACHE
 
-sudo apt-get install apache2 -y
+clear
+sudo aptitude install apache2 -y
+clear
 ip addr show eth0 | grep inet | awk '{ print $2; }' | sed 's/\/.*$//'
 
 # MYSQL
 
-sudo apt-get install mysql-server php5-mysql -y
+clear
+sudo aptitude install mysql-server php5-mysql -y
+clear
 sudo mysql_install_db
 # sudo mysql_secure_installation
 
 # PHP
 
-sudo apt-get install software-properties-common -y
-sudo apt-get install python-software-properties -y
-sudo apt-get install php5 libapache2-mod-php5 php5-mcrypt -y
+clear
+sudo aptitude install software-properties-common -y
+clear
+sudo aptitude install python-software-properties -y
+clear
+sudo aptitude install php5 libapache2-mod-php5 php5-mcrypt -y
+clear
 sudo service apache2 restart
+clear
 sudo aptitude install php5-cgi -y
+clear
 sudo aptitude install php5-cli -y
-sudo apt-get install php-apc -y
+clear
+sudo aptitude install php-apc -y
+clear
 sudo aptitude install php5-common -y
+clear
 sudo aptitude install php5-curl -y
+clear
 sudo aptitude install php5-dbg -y
+clear
 sudo aptitude install php5-gd -y
+clear
 sudo aptitude install php5-gmp -y
+clear
 sudo aptitude install php5-mysql -y
+clear
 sudo aptitude install php5-odbc -y
+clear
 sudo aptitude install php5-pgsql -y
+clear
 sudo aptitude install php5-geoip -y
+clear
 sudo aptitude install php5-imagick -y
+clear
 sudo aptitude install php5-imap -y
+clear
 sudo aptitude install php5-mcrypt -y
+clear
 sudo aptitude install php5-memcache -y
+clear
 sudo aptitude install php5-memcached -y
-sudo aptitude install php5-mysqlnd -y
-sudo aptitude install php5-xcache -y
+clear
+# Conflicts: php5-mysql but 5.5.9+dfsg-1ubuntu4.11 is to be installed.
+# sudo aptitude install php5-mysqlnd -y
+clear
+# sudo aptitude install php5-xcache -y
+clear
 sudo aptitude install php5-xdebug -y
 
-curl -sS https://getcomposer.org/installer | php
-mv composer.phar /usr/local/bin/composer
+sudo curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
+sudo mv composer.phar /usr/local/bin
 
-lynx http://localhost/
 sudo chmod -R 777 /var/www
-touch /var/www/index.php
-echo  "<?php phpinfo(); ?>" > /var/www/index.php 
-rm /var/www/index.html
-lynx http://localhost/ # should show php info
+touch /var/www/html/index.php
+echo  "<?php phpinfo(); ?>" > /var/www/html/index.php 
+rm /var/www/html/index.html
 sudo service apache2 restart
+lynx http://localhost/ # should show php info
 
+echo 'export PS1="\e[0;35m[\u@\h \W]\$ \e[m "' >> ~/.bash_profile
+echo 'alias composer="php /usr/local/bin/composer"' >> ~/.bash_profile
+echo 'alias mc="mc -b"' >> ~/.bash_profile
 
+clear
+php5 -v
+echo "
+
+Everything's installed, boss. Re-login, please.
+
+"
